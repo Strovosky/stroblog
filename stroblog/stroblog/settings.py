6e39f939d2c9cec8509ee727f3b832b78c0ec425
Strovosky <strovosky@gmail.com>
2022-05-25 19:18:50 -0500

    Updated settings wit environment variables

--- stroblog/stroblog/settings.py
+++ stroblog/stroblog/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ow))ucu4mj^v8frvep8@1#tex5(15brw*%(@kkjf#%v3sp1i(3'
+# SECRET_KEY = 'django-insecure-ow))ucu4mj^v8frvep8@1#tex5(15brw*%(@kkjf#%v3sp1i(3'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['stroblog.herokuapp.com']
 
 
 # Application definition
